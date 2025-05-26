844f79bfb4bf34df75a09ae6ae378bfbfcd73dcf
GrafEnters <graf.enters@gmail.com>
2025-05-26 19:14:30 +0200

    chenges and packages

--- Assets/Scripts/CellView.cs
+++ Assets/Scripts/CellView.cs
@@ -1,6 +1,20 @@
+using System;
 using UnityEngine;
 
 public class CellView : MonoBehaviour {
     [field: SerializeField]
     public CellType CellType { get; private set; }
+
+    [SerializeField]
+    private GameObject _objectsContainer;
+
+    private void Start() {
+        if (_objectsContainer) {
+            RandomRotateObjects();
+        }
+    }
+
+    private void RandomRotateObjects() {
+        _objectsContainer.transform.Rotate(Vector3.up, UnityEngine.Random.Range(0f, 360f));
+    }
 }
