465bf70e51bf9624df45f2f6bf7f59b53cc55fcf
abc <bogdanseredin1@gmail.com>
2025-07-08 20:28:31 +0200

    open meta after win, watch add button if need health after lose

---
+++ Assets/Scripts/Dialogs/RecoveryHealthDialog.cs
@@ -0,0 +1,21 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RecoveryHealthDialog : DialogBase
+{
+    public void RecoveryHealthWithAds()
+    {
+        if (StorageManager.GameDataMain.HealthCount > 0)
+            MainManager.Instance.Restart();
+        else
+        {
+            Hide().Forget();
+            AdsManager.Instance.ShowRewarded(() =>
+            {
+                StorageManager.GameDataMain.HealthCount++;
+                SceneManager.LoadScene("GameScene");
+            }).Forget();
+        }
+    }
+}
--- Assets/Scripts/GoalView.cs
+++ Assets/Scripts/GoalView.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GoalView : MonoBehaviour {
@@ -18,6 +19,7 @@ public class GoalView : MonoBehaviour {
     public static GoalView Instance;
     public bool _isGameEnded { get;private set;}
     private Tween _currentTween;
+    [SerializeField] private TMP_Text _loseRestartText;
 
     private void Awake() {
         Instance = this;
@@ -28,36 +30,41 @@ public class GoalView : MonoBehaviour {
         WinAnimation();
        
         _isGameEnded = true;
-        _winState.gameObject.SetActive(true);
+       // _winState.gameObject.SetActive(true);
         _loseState.gameObject.SetActive(false);
     }
 
     public void SetLoseState() {
         if (_isGameEnded) return;
+        if (StorageManager.GameDataMain.HealthCount <= 0)
+            _loseRestartText.text = "Watch add and recovery 1 energy";
+        else
+            _loseRestartText.text = "Restart";
         LoseAnimation();
         _isGameEnded = true;
-        _winState.gameObject.SetActive(false);
+       // _winState.gameObject.SetActive(false);
         _loseState.gameObject.SetActive(true);
     }
 
-    private void WinAnimation() {
+    private void WinAnimation()
+    {
         _currentTween = DOTween.Sequence()
-            .Append(GameUI.Instance.BgTasksImage.DOAnchorPosY(GameUI.Instance.BgTasksImage.anchoredPosition.y + 370, 1f))
-            .Append(GameUI.Instance.OpenedDoorEndGame.DOMoveY(GameUI.Instance.OpenedDoorEndGame.position.y + 2.1f, 0.7f))
-            .Append(GameUI.Instance.OpenedDoorEndGame.DOMoveY(GameUI.Instance.OpenedDoorEndGame.position.y + 2f, 0.07f))
-            .Append(GameUI.Instance.OpenedDoorEndGame.DOMoveY(GameUI.Instance.OpenedDoorEndGame.position.y + 2.25f, 0.1f))
-            .Join(_winState.DOAnchorPosY(_winState.anchoredPosition.y - 500, 0.6f))
-            .Append(GameFieldManager.Instance.CameraContainer.DOMoveZ(GameFieldManager.Instance.CameraContainer.position.z + 5, 3f));
+            .Append(
+                GameUI.Instance.BgTasksImage.DOAnchorPosY(GameUI.Instance.BgTasksImage.anchoredPosition.y + 370, 1f))
+            .Append(
+                GameUI.Instance.OpenedDoorEndGame.DOMoveY(GameUI.Instance.OpenedDoorEndGame.position.y + 2.3f, 0.7f))
+            .Append(GameUI.Instance.OpenedDoorEndGame.DOMoveY(GameUI.Instance.OpenedDoorEndGame.position.y + 2.2f, 0.07f))
+            .Append(GameUI.Instance.OpenedDoorEndGame.DOMoveY(GameUI.Instance.OpenedDoorEndGame.position.y + 2.45f,
+                0.1f))
+            .Append(GameFieldManager.Instance.CameraContainer.DOMoveZ(
+                GameFieldManager.Instance.CameraContainer.position.z + 5, 3f))
+            .OnComplete(() => SceneManager.LoadScene("MetaScene"));
     }
 
     private void LoseAnimation() {
         _currentTween = DOTween.Sequence()
             .Append(GameUI.Instance.BgTasksImage.DOAnchorPosY(GameUI.Instance.BgTasksImage.anchoredPosition.y + 370, 1f))
-            .Append(_loseState.DOAnchorPosY(_loseState.anchoredPosition.y - 500, 0.6f));
-    }
-
-    public void StartAgain() {
-        MainManager.Instance.Restart();
+            .Append(_loseState.DOAnchorPosY(_loseState.anchoredPosition.y - 800, 0.6f));
     }
 
     public void ExitGame() {
