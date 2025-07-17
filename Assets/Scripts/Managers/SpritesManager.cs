0340292a4e5a543c57dbb120e9cf6f722724f886
flutterl0ver <yurchuk.pasha10@mail.ru>
2025-07-17 15:14:18 +0200

    new sprites manager

--- Assets/Scripts/Managers/SpritesManager.cs
+++ Assets/Scripts/Managers/SpritesManager.cs
@@ -1,23 +1,17 @@
-using System.Collections.Generic;
+using AYellowpaper.SerializedCollections;
 using UnityEngine;
 
 public class SpritesManager : MonoBehaviour {
     public static SpritesManager Instance;
 
-    [SerializeField]
-    private Sprite[] _sprites;
-
-    private Dictionary<string, Sprite> _spritesDict = new();
+    [SerializedDictionary("Resource Type", "Sprite")]
+    public SerializedDictionary<ResourceType, Sprite> ResourcesSprites;
     
     private void Awake() {
         Instance = this;
-
-        foreach (Sprite sprite in _sprites) {
-            _spritesDict[sprite.name] = sprite;
-        }
     }
 
-    public Sprite GetSprite(string spriteName) {
-        return _spritesDict[spriteName];
+    public Sprite GetSprite(ResourceType resource) {
+        return ResourcesSprites[resource];
     }
 }
