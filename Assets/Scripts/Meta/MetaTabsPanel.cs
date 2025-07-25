8cc914d2a4b1e378547c9284b972ba309082ec6b
flutterl0ver <yurchuk.pasha10@mail.ru>
2025-07-25 16:11:51 +0200

    new meta ui

---
+++ Assets/Scripts/Meta/MetaTabsPanel.cs
@@ -0,0 +1,63 @@
+using System;
+using AYellowpaper.SerializedCollections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MetaTabsPanel : MonoBehaviour {
+    [SerializeField]
+    private RectTransform _activeButtonExample, _notActiveButtonExample;
+    
+    [SerializeField]
+    private Sprite _activeSprite, _notActiveSprite;
+
+    [SerializedDictionary]
+    public SerializedDictionary<MetaTab, RectTransform> _tabsButtons;
+
+    private Vector2 _activeSize, _notActiveSize;
+    private MetaTab _selectedTab = MetaTab.Rule;
+    
+    private void Start() {
+        _activeSize = _activeButtonExample.sizeDelta;
+        _notActiveSize = _notActiveButtonExample.sizeDelta;
+    }
+    
+    public void OpenProfile() {
+        if (_selectedTab == MetaTab.Profile) return;
+
+        ChangeTab(MetaTab.Profile);
+    }
+
+    public void OpenShop() {
+        if (_selectedTab == MetaTab.Shop) return;
+        
+        ChangeTab(MetaTab.Shop);
+    }
+    
+    public void OpenRule() {
+        if (_selectedTab == MetaTab.Rule) return;
+
+        ChangeTab(MetaTab.Rule);
+    }
+    
+    public void OpenEvents() {
+        if (_selectedTab == MetaTab.Events) return;
+
+        ChangeTab(MetaTab.Events);
+    }
+
+    private void ChangeTab(MetaTab newTab) {
+        _tabsButtons[_selectedTab].sizeDelta = _notActiveSize;
+        _tabsButtons[_selectedTab].GetComponent<Image>().sprite = _notActiveSprite;
+        _tabsButtons[newTab].sizeDelta = _activeSize;
+        _tabsButtons[newTab].GetComponent<Image>().sprite = _activeSprite;
+
+        _selectedTab = newTab;
+    }
+}
+
+public enum MetaTab {
+    Profile,
+    Shop,
+    Rule,
+    Events
+}
