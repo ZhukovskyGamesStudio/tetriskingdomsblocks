e217628b42bd319980a859d26e3242609192d171
abc <bogdanseredin1@gmail.com>
2025-07-26 13:18:19 +0200

    floating resources after upgrade and collect( only to village)

--- Assets/Scripts/Utils/UIAnimationsUtils.cs
+++ Assets/Scripts/Utils/UIAnimationsUtils.cs
@@ -5,47 +5,32 @@ using Cysharp.Threading.Tasks.Triggers;
 
 public static class UIAnimationsUtils
 {
-   public static void FromPointToPointAnimation(Action onComplete, RectTransform uiElement, Vector2 startWorldPos, Vector2 endWorldPos)
-    {
-        //make this method with "for" for multiply elements and with images pool
-        Canvas canvas = uiElement.GetComponentInParent<Canvas>();
-        if (canvas == null) return;
+    public static void FromPointToPointAnimation(int needCount, ResourceType resourceType,Vector2 startWorldPos,
+        Vector2 endWorldPos) {
 
-        Vector2 startAnchoredPos = WorldToCanvasPosition(canvas, startWorldPos);
-        Vector2 endAnchoredPos = WorldToCanvasPosition(canvas, endWorldPos);
-        
-        uiElement.anchoredPosition = startAnchoredPos;
-        
-        float randomAngle = UnityEngine.Random.Range(-30f, 30f) * Mathf.Deg2Rad;
-        float distance = Vector2.Distance(startAnchoredPos, endAnchoredPos) * 0.3f;
-        
-        Vector2 dir = (startAnchoredPos - endAnchoredPos).normalized;
-        Vector2 deviatedDir = new Vector2(
-            dir.x * Mathf.Cos(randomAngle) - dir.y * Mathf.Sin(randomAngle),
-            dir.x * Mathf.Sin(randomAngle) + dir.y * Mathf.Cos(randomAngle)
-        );
-        
-        Vector2 midPoint = startAnchoredPos + deviatedDir * distance;
-        
-        Sequence sequence = DOTween.Sequence();
-        
-        sequence.Append(uiElement.DOAnchorPos(midPoint, 0.3f).SetEase(Ease.OutQuad));
-        
-        sequence.Append(uiElement.DOAnchorPos(endAnchoredPos, 0.4f).SetEase(Ease.InQuad));
-        
-        sequence.OnComplete(() => onComplete?.Invoke());
-    }
+        Debug.Log($"{needCount} count {startWorldPos} to {endWorldPos}");
+        float angleStep = 70f / needCount;
+        for (int i = 0; i < needCount; i++) {
+            var uiElement = MetaUI.Instance.ShowFloatingText();
+            uiElement.text = $"<sprite name={resourceType}>";
+            uiElement.transform.position = startWorldPos;
+            float randomAngle =  (i - (float)needCount / 2)*angleStep * Mathf.Deg2Rad;
+            //UnityEngine.Random.Range(-30f, 30f)
+            float distance = Vector2.Distance(startWorldPos, endWorldPos) * 0.2f;
 
-    private static Vector2 WorldToCanvasPosition(Canvas canvas, Vector3 worldPosition)
-    {
-        RectTransform canvasRect = canvas.GetComponent<RectTransform>();
-        Camera camera = /*canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null :*/ Camera.main;
-        
-        Vector2 viewportPosition = camera.WorldToViewportPoint(worldPosition);
-        
-        return new Vector2(
-            (viewportPosition.x * canvasRect.sizeDelta.x) - (canvasRect.sizeDelta.x * 0.5f),
-            (viewportPosition.y * canvasRect.sizeDelta.y) - (canvasRect.sizeDelta.y * 0.5f)
-        );
+            Vector2 dir = (startWorldPos - endWorldPos).normalized;
+            Vector2 deviatedDir = new Vector2(dir.x * Mathf.Cos(randomAngle) - dir.y * Mathf.Sin(randomAngle),
+                dir.x * Mathf.Sin(randomAngle) + dir.y * Mathf.Cos(randomAngle));
+
+            Vector2 midPoint = startWorldPos + deviatedDir * distance;
+
+            Sequence sequence = DOTween.Sequence();
+
+            sequence.Append(uiElement.transform.DOMove(midPoint, 0.8f).SetEase(Ease.OutQuad));
+
+            sequence.Append(uiElement.transform.DOMove(endWorldPos, 0.5f).SetEase(Ease.InQuad));
+
+            sequence.OnComplete(() => MetaUI.Instance.ReleaseFloatingText(uiElement));
+        }
     }
 }
