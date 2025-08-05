6882edefd302d36badd4cd2260624a14e9fe0e7a
Graf Enters <graf.enters@gmail.com>
2025-08-06 00:47:48 +0200

    lootbox wip

--- Assets/Scripts/Dialogs/LootboxDialog.cs
+++ Assets/Scripts/Dialogs/LootboxDialog.cs
@@ -4,22 +4,34 @@ using TMPro;
 using UnityEngine;
 
 public class LootboxDialog : DialogBase {
+    private static readonly int Open = Animator.StringToHash("open");
+
     [SerializeField]
     private GameObject _openState, _continueState;
 
+    [SerializeField]
+    private Animator _chestAnimation;
+
     private PieceData _rewardingPiece;
-    
+
     public override void SetData(object data) {
         Data dialogData = data as Data;
 
         _rewardingPiece = dialogData.RewardingPiece;
-        
+
         _openState.SetActive(true);
         _continueState.SetActive(false);
     }
 
     public void ClickOpen() {
         _openState.SetActive(false);
+        _chestAnimation.SetTrigger(Open);
+        WaitForOpen().Forget();
+    }
+    
+    private async UniTask WaitForOpen() {
+        await UniTask.WaitUntil(() => _chestAnimation.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f);
+        SetContinueState();
     }
 
     public void SetContinueState() {
