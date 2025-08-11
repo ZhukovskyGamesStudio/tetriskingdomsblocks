d2121de788686ee13d084041aa48a41f07d0fea6
fluttershY_ <yurchuk.pasha10@mail.ru>
2025-08-11 17:17:03 +0200

    remover cell types tables

--- Assets/Scripts/ScriptableObjects/Configs/CellsAndResourceTypesTableConfig.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CellsAndResourceTypesTableConfig", menuName = "Scriptable Objects/CellsAndResourceTypesTableConfig")]
-public class CellsAndResourceTypesTableConfig : ScriptableObject {
-    [field: SerializeField]
-    public CellType[] CellsToSpawn { get; set; }
-}
