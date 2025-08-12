adb55ffae446c61e4eefc92d0f020f48414df68d
Graf Enters <graf.enters@gmail.com>
2025-08-12 14:00:20 +0200

    refactored levelOne tutorial final

--- Assets/Scripts/Tutorial/LevelOneTutorial.cs
+++ Assets/Scripts/Tutorial/LevelOneTutorial.cs
@@ -1,45 +1,18 @@
 using System;
 using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
-using DG.Tweening;
 using ScriptableObjects;
-using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class LevelOneTutorial : MonoBehaviour {
     [SerializeField]
-    private RectTransform[] _holeImages;
+    private RectTransform _pieceContainer, _goalViewContainer;
 
     [SerializeField]
-    private RectTransform _blackBGImage;
+    private List<Vector3Int> _firstStepCells, _secondStepCells;
 
     [SerializeField]
-    private TMP_Text _tutorialText;
-
-    [SerializeField]
-    private Image _fingerImage;
-
-    [SerializeField]
-    private string _firstTutorialText;
-
-    [SerializeField]
-    private string _secondTutorialText;
-
-    [SerializeField]
-    private string _thirdTutorialText;
-
-    [SerializeField]
-    private Tween _currentTween;
-
-    [SerializeField]
-    private TutorialHoleHelper _holeHelper;
-
-    [SerializeField]
-    private List<Vector3Int> _firstStepCells, _secondStepCells, _thirdStepCells;
-
-    [SerializeField]
-    private SpotlightAnimConfig _step1Config, _step2Config, _step3Config;
+    private SpotlightAnimConfig _step1Config, _step2Config;
 
     [SerializeField]
     private bool _canSkipTutorial;
@@ -51,7 +24,10 @@ public class LevelOneTutorial : MonoBehaviour {
         ShowFirstStepTutorial();
         SetHolesPositions();
 
-        SpotlightsManager.Instance.StartFingerAnimation(_holeImages[0].transform.position, (Vector2)Camera.main!.WorldToScreenPoint(new Vector3(3.5f, 0, 3.5f)));
+        _pieceContainer.gameObject.SetActive(true);
+        _goalViewContainer.gameObject.SetActive(false);
+        SpotlightsManager.Instance.StartFingerAnimation(_pieceContainer.transform.position,
+            (Vector2)Camera.main!.WorldToScreenPoint(new Vector3(3.5f, 0, 3.5f)));
     }
 
     private void Update() {
@@ -70,30 +46,14 @@ public class LevelOneTutorial : MonoBehaviour {
 
     private void TrySkipStep() {
         if (_canSkipTutorial) {
-            /*  if (_tutorialStep == 2)
-                  HideSecondStepTutorial();*/
             if (_tutorialStep == 3)
-                HideThirdStepTutorial();
+                HideThirdStepTutorial().Forget();
         }
     }
 
     public void SetHolesPositions() {
         var posHoleSecond = (Vector2)Camera.main.WorldToScreenPoint(NextPiecesView.Instance._piecesContainers[0].transform.position);
-        // Присваиваем позицию UI-элементу
-        //ar posHoleFirst = GameUI.Instance._tasksContainer.position;
-
-        //_holeImages[1].transform.SetParent(_holeHelper._holesContainer,true);
-        //_holeImages[0].transform.SetParent(_holeHelper._holesContainer,true);
-        //_holeImages[2].transform.SetParent(_holeHelper._holesContainer,true);
-
-        //_holeImages[1].position = posHoleFirst;
-        _holeImages[0].transform.position = posHoleSecond;
-        _holeImages[2].transform.position = (Vector2)Camera.main.WorldToScreenPoint(new Vector3(4, 0, 3.5f));
-        _tutorialText.transform.position = (Vector2)Camera.main.WorldToScreenPoint(new Vector3(4, 0, 2));
-        _fingerImage.transform.position = _holeImages[0].transform.position;
-        _holeImages[0].gameObject.SetActive(true);
-        _holeImages[1].gameObject.SetActive(false);
-        _holeImages[2].gameObject.SetActive(false);
+        _pieceContainer.transform.position = posHoleSecond;
     }
 
     private void ShowFirstStepTutorial() {
@@ -102,16 +62,15 @@ public class LevelOneTutorial : MonoBehaviour {
         SpotlightsManager.Instance.SpotlightWithText.ShowSpotlight(SpotlightsManager.Instance.CenterScreenAnchor, _step1Config);
         TutorialHoleHelper.DestroyHoles();
         TutorialHoleHelper.SpawnHoles(_firstStepCells);
-        _tutorialText.text = _firstTutorialText;
         HighlightCurrentPiece();
     }
 
-    private GameObject _pieceCellsContainer;
+    private GameObject _nextPiecesContainer;
 
     private void HighlightCurrentPiece() {
         PieceView piece = FindAnyObjectByType<PieceView>();
-        _pieceCellsContainer = piece._cellsContainer.gameObject;
-        TutorialHoleHelper.HighlightObjects(new List<GameObject> { _pieceCellsContainer });
+        _nextPiecesContainer = piece._cellsContainer.gameObject;
+        TutorialHoleHelper.HighlightObjects(new List<GameObject> { _nextPiecesContainer });
     }
 
     public void HideFirstStepTutorial(Vector2Int pos, bool[,] cells) {
@@ -127,51 +86,25 @@ public class LevelOneTutorial : MonoBehaviour {
         TutorialHoleHelper.DestroyHoles();
         GameFieldManager.Instance.OnCellPlaced -= HideFirstStepTutorial;
         GameFieldManager.Instance.ClearAllLockedCells();
-        _currentTween.Kill();
         _canSkipTutorial = true;
-        _holeImages[0].gameObject.SetActive(false);
-        _fingerImage.gameObject.SetActive(false);
-        _blackBGImage.gameObject.SetActive(false);
-        _tutorialText.gameObject.SetActive(false);
+        _pieceContainer.gameObject.SetActive(false);
+        
+        //TODO дождаться анимации перелёта ресурсов от клеток в счётчики
         await UniTask.Delay(TimeSpan.FromSeconds(1));
         ShowThirdStepTutorial();
     }
 
-    public void ShowSecondStepTutorial() {
-        TutorialHoleHelper.HighlightObjects(new List<GameObject> { _pieceCellsContainer });
-        TutorialHoleHelper.SpawnHoles(_secondStepCells);
-        _tutorialStep = 2;
-        Time.timeScale = 0;
-        _tutorialText.text = _secondTutorialText;
-        _holeImages[2].gameObject.SetActive(true);
-    }
-
-    public void HideSecondStepTutorial() {
-        TutorialHoleHelper.DestroyHoles();
-        _holeImages[2].gameObject.SetActive(false);
-        Time.timeScale = 1;
-        _canSkipTutorial = false;
-        _tutorialText.gameObject.SetActive(false);
-    }
-
     public void ShowThirdStepTutorial() {
-        SpotlightsManager.Instance.SpotlightWithText.ShowSpotlight( GameUI.Instance.GoalView.transform, _step2Config);
-        //_blackBGImage.gameObject.SetActive(true);
-        //_tutorialText.transform.position =
-        //    new Vector3(GameUI.Instance._tasksContainer.position.x, GameUI.Instance._tasksContainer.position.y - 100f, 0);
-        TutorialHoleHelper.SpawnHoles(_thirdStepCells);
+        SpotlightsManager.Instance.SpotlightWithText.ShowSpotlight(GameUI.Instance.GoalView.transform, _step2Config);
         _tutorialStep = 3;
-        //Time.timeScale = 0;
         _canSkipTutorial = true;
-        //_tutorialText.text = _thirdTutorialText;
-        //_tutorialText.gameObject.SetActive(true);
-        _holeImages[1].gameObject.SetActive(true);
+        _goalViewContainer.gameObject.SetActive(true);
     }
 
     public async UniTask HideThirdStepTutorial() {
         TutorialHoleHelper.DestroyHoles();
         Time.timeScale = 1;
-        _holeImages[1].gameObject.SetActive(false);
+        _goalViewContainer.gameObject.SetActive(false);
         DestroyTutorial();
         await SpotlightsManager.Instance.SpotlightWithText.HideSpotlight();
         ShowWitch();
@@ -184,13 +117,8 @@ public class LevelOneTutorial : MonoBehaviour {
     }
 
     public void DestroyTutorial() {
-        _currentTween.Kill();
-        foreach (var hole in _holeImages) {
-            Destroy(hole.gameObject);
-        }
-
-        Destroy(_blackBGImage.gameObject);
-        Destroy(_fingerImage.gameObject);
+        Destroy(_nextPiecesContainer.gameObject);
+        Destroy(_goalViewContainer.gameObject);
 
         Destroy(gameObject);
     }
