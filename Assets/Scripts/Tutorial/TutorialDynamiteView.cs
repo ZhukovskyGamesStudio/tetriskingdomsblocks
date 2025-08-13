6c3a2086accdf8f6b53c81d1b23ea26de1cefb61
Graf Enters <graf.enters@gmail.com>
2025-08-13 10:21:03 +0200

    mechanics tutorial

--- Assets/Scripts/Tutorial/TutorialDynamiteView.cs
+++ Assets/Scripts/Tutorial/TutorialDynamiteView.cs
@@ -1,13 +1,13 @@
 using UnityEngine.UI;
 
-public class TutorialHammerView : BoosterTutorialView {
+public class TutorialDynamiteView : BoosterTutorialView {
     protected override void Init() {
-        StorageManager.GameDataMain.HummerCount = 5;
+        StorageManager.GameDataMain.DynamiteCount = 5;
 
         if (BoostersManager.Instance != null) {
             GameUI.Instance.GameBoostersButtons.UpdateCounters(StorageManager.GameDataMain);
         }
     }
 
-    protected override Button BoosterButton => GameUI.Instance.GameBoostersButtons._hummerButton;
+    protected override Button BoosterButton => GameUI.Instance.GameBoostersButtons._dinamyteButton;
 }
