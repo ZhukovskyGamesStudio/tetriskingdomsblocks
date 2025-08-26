1db0a5d401b7f078a562b8c4fb5dad956b66120e
Graf Enters <graf.enters@gmail.com>
2025-08-26 17:04:36 +0200

    changed inaps ids

--- Assets/Scripts/InApps/InApsIds.cs
+++ Assets/Scripts/InApps/InApsIds.cs
@@ -2,14 +2,14 @@
 using System.Collections.Generic;
 
 public static class InApsIds {
-    public static string Special = "tetris.kingdoms.blocks.special";
-    public static string Bundle1 = "tetris.kingdoms.blocks.bundle1";
-    public static string Bundle2 = "tetris.kingdoms.blocks.bundle2";
-    public static string Bundle3 = "tetris.kingdoms.blocks.bundle3";
-    public static string Hearts = "tetris.kingdoms.blocks.hearts";
-    public static string Coins = "tetris.kingdoms.blocks.coins";
+    public static string Special = "blast.merge.match.tycoon.special";
+    public static string Bundle1 = "blast.merge.match.tycoon.bundle1";
+    public static string Bundle2 = "blast.merge.match.tycoon.bundle2";
+    public static string Bundle3 = "blast.merge.match.tycoon.bundle3";
+    public static string Hearts = "blast.merge.match.tycoon.hearts";
+    public static string Coins = "blast.merge.match.tycoon.coins";
 
-    public static string NoAds = "tetris.kingdoms.blocks.noads";
+    public static string NoAds = "blast.merge.match.tycoon.noads";
 
     public static Dictionary<InApsTypes, string> InAps = new() {
         { InApsTypes.Special, Special },
