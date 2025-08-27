7329fa16b3b276c1812aa6d252a4901a1c89938e
Graf Enters <graf.enters@gmail.com>
2025-08-27 03:42:39 +0200

    integrations work

---
+++ Assets/Scripts/UnityPurchasing/generated/AppleStoreKitTestTangle.cs
@@ -0,0 +1,18 @@
+// WARNING: Do not modify! Generated file.
+
+namespace UnityEngine.Purchasing.Security {
+    public class AppleStoreKitTestTangle
+    {
+        private static byte[] data = System.Convert.FromBase64String("ltRIWp3ClFwpM2uJuf4YPTDW/Df6wfj38qT19+LzpKLA5sHk9/jbd8GuwODB/vfypPXy/fmihZ6DlLqY08H89/jbd7l3Bvzw8PD08fJz8P66Ykr7CVDQzzmQKNU5r+6EzytiKsDrYHFIThT9Os1K/7WT1wv12Ee4fRCAKru0AtzoZ/KncFe1H4SGqqIWnhHqF/yZPNSfMof5BLv6X4uFzXSJuT8xNOPgm/79X970O56LlY7Rklpma9oy2ykhQxAkq/zI6p6W2VjB+/f52vfw9PT28vLB/Pf423e5dzG22boaqzDa+KfdVFv48orDwC0Z7R74eYmUP3ADfIMW9uuf1+MGw0A1WPpCPpP+CkVNBip1r9katvwAHMDgwf738qT1+v35ooWeg5S6mIXABvzw8Pr08fJz8PDxQ/ERzQAZGJMzCheu4zDz8vDx8FLKwcjB/vfypD6JXaxLz66h2q3YfSwzJtvmLtNF86SiwObB5Pf423e5dwb88Pjw5/l6XQRuQfjJUp+KOteh++sQeCCDh/rIvTq+nzFCrkZkS0hZrLuFWh6exMPCxKvm/MXBwcLDxsDGxMPCxKtMMSfssW+mSMlhhoejI1m6gZ9Jlv35ooWeg5S6mIXA+sH49/Kk9ffi43IndomcaEyvQvsRaT8HGzJ/nJg+lc2ccajCfTvH99vg6ny4P/kOl2gKQnukbh7s2Umu2yCCbsS2xkq3/ErJYZr3NQ38FGShPGA5S9QFqTjbd7l3Bvzw8Pr08cGuwODB/vfypLl3Bvzw+PDn+aKFnoOUupiFwXPw8cFz8Pvzc/Dw8SqOYc3dgv4qZtf18v35ooWeg5S6mIXA4MH+9/Kk9ezi8PAO9fTB8vDwDsH/9/Kk7P7whcDgwf738qT1+/35ooWeg5S6mIUaRrFwzHbMgaAgNax8VC2Om6nrLyKH4QMBmB08k2iACJF7dbnJNUx6wXPyhcFz861R8vPw8/Pw8MH89/iwVI/4fEvMzDyBpikcHiQrhjli9aKFnoOUupiFwe/m/MPBwcXBwMDGkYfGox0OXeis/eTee/4EeDkm2uwmFYlZXlmcVjArmdyXPT7Y+qHdEt2EF71A4yIMEu6v/IxJqSZRjRWO8A719fLz83XB5/fypOzU8PAO9f37/fmihZ6DlLqYhcDgwf738qT1+u8d2mD0Vsy+");
+        private static int[] order = new int[] { 35,11,8,13,15,16,24,17,38,40,30,37,22,18,30,31,27,25,27,19,20,25,32,31,41,36,33,31,37,32,30,38,37,40,42,42,42,42,41,43,40,43,42,43,44 };
+        private static int key = 241;
+
+        public static readonly bool IsPopulated = true;
+
+        public static byte[] Data() {
+        	if (IsPopulated == false)
+        		return null;
+            return Obfuscator.DeObfuscate(data, order, key);
+        }
+    }
+}
