3175f0bbdf377b12b83f55bf46fd8028b777f94d
Graf Enters <graf.enters@gmail.com>
2025-10-01 09:31:59 +0300

    something something ta ta ta

--- Assets/Scripts/Analytics/AppsflyerAnalyticsProvider.cs
+++ Assets/Scripts/Analytics/AppsflyerAnalyticsProvider.cs
@@ -1,11 +1,17 @@
 #if APPSFLYER
+using System;
 using System.Collections.Generic;
 using AppsFlyerSDK;
+using UnityEngine;
 
 public class AppsflyerAnalyticsProvider : IAnalyticsProvider {
     private const string DEV_KEY = "pWT7Si9aYsLLGEDwgWczDh";
 
     public AppsflyerAnalyticsProvider() {
+        AppsFlyer.OnRequestResponse += AppsFlyerOnRequestResponse;
+        AppsFlyer.OnInAppResponse += AppsFlyerOnInAppResponse;
+        AppsFlyer.setIsDebug(true);
+        Debug.Log("Appsflyer initialized");
         AppsFlyer.initSDK(DEV_KEY, ""); // app_id for iOS, empty string for Android
         AppsFlyer.startSDK();
     }
@@ -17,7 +23,18 @@ public class AppsflyerAnalyticsProvider : IAnalyticsProvider {
             sentData[kvp.Key] = kvp.Value != null ? kvp.Value.ToString() : string.Empty;
         }
 
+        Debug.Log("Appsflyer SendEvent " + eventName);
         AppsFlyer.sendEvent(eventName, sentData);
+    }
+
+    private void AppsFlyerOnRequestResponse(object sender, EventArgs e) {
+        var args = e as AppsFlyerRequestEventArgs;
+        Debug.Log("AppsFlyerOnRequestResponse status code " + args?.statusCode);
+    }
+
+    private void AppsFlyerOnInAppResponse(object sender, EventArgs args) {
+        var afArgs = args as AppsFlyerRequestEventArgs;
+        Debug.Log("AppsFlyerOnRequestResponse status code " + afArgs?.statusCode);
     }
 }
 #endif
